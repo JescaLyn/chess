987467ddc1ee2ae8b9db3b4417947e6b8e12ea9c
App Academy Student <student@appacademy.io>
2016-07-18 12:15:48 -0700

    Setting up Board class

---
+++ board.rb
@@ -0,0 +1,16 @@
+require-relative "tile"
+
+class Board
+  def initialize()
+    @grid = Array.new(8) { Array.new(8) }
+    self.populate
+  end
+
+  def populate
+
+  end
+
+  def move(start_pos, end_pos)
+
+  end
+end
